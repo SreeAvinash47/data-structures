80aa97217a395e6f57500c7b47f0b964c019ee4c
SreeAvinash47 <sreeavinash.madiki@gmail.com>
2020-06-22 07:20:56 +0530

    Print all subarray with 0 sum

--- arrays/print_all_sub_arrays_with_0_sum.js
+++ arrays/print_all_sub_arrays_with_0_sum.js
@@ -1,19 +1,26 @@
 // Print all the sub arrays with sum = 0
 function get_all_subarrays(inp, Sum=0){
     const hash = {}; // {'0':[[0,4], [4,7]]}
-    const sum=0
+    const all_subarrays=[]
+    let sum=0
     let index=-1
-    for (let ele of inp){
+    hash[0]=[-1]
+    inp.forEach((ele, ind)=>{
         sum += ele
-        if(sum in Object.keys(hash).map(item=>String(item)){
-        }
-    }
-
+        if(Object.keys(hash).find(item=>item==sum)){
+            content = hash[sum]
+            for(let cid in content){
+                all_subarrays.push(inp.slice(content[cid]+1, ind+1))
+            }
+            hash[sum].push(ind)
+        }else hash[sum]=[ind]
+    })
+    return all_subarrays
 }
 function main(){
     const inp = [3,4,-7,3,1,3,1,-4,-2,-2];
     const sum=0;
     const out = get_all_subarrays(inp, sum)
-    console.log(`All subarrays with sum=0 is ${out}`)
+    console.log(`All subarrays with sum=0 is `, out)
 }
 main()
